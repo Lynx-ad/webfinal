b8a2b38935fb5de33ceda54468b0c9f037bf2d0d
lynx-ad <108929130+Lynx-ad@users.noreply.github.com>
2023-06-10 22:27:32 +0800

    <commit_message>

---
+++ video.js
@@ -0,0 +1,20 @@
+function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    return rect.top <= windowHeight && rect.bottom >= 0;
+}
+
+// Autoplay video when scrolled into view
+function autoplayVideoOnScroll() {
+    var video = document.getElementById('video');
+    var videoSection = document.querySelector('.section-2');
+
+    if (isElementInViewport(videoSection)) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// Attach autoplayVideoOnScroll to scroll event
+window.addEventListener('scroll', autoplayVideoOnScroll);
